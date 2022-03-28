cfc4b8b485039dc5bea7f91d06bf87f2b7b2e709
youthfulhps <ybh942002@gmail.com>
2022-03-28 18:29:28 +0900

    Implemented POST memo api

--- src/apis/Memo/apis.ts
+++ src/apis/Memo/apis.ts
@@ -1,5 +1,10 @@
+import { PostMemoPayload } from ".";
 import { API } from "../base";
 
 export const getMemos = () => {
   return API.get("/memos");
+};
+
+export const postMemo = ({ title, content }: PostMemoPayload) => {
+  return API.post("/memos", { title, content });
 };
